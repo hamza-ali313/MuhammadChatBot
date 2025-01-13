3ea21fa3ae3948f9017e5d948fecd76759df2fc0
hamza ali <70458340+hamza-ali313@users.noreply.github.com>
2025-01-13 09:16:30 +0500

    Create long_responses.py

---
+++ long_responses.py
@@ -0,0 +1,16 @@
+# long_responses.py
+
+R_ADVICE = (
+    "If I were you, I would go to the internet and type exactly what you wrote there!"
+)
+
+R_EATING = (
+    "I don't like eating anything because I'm a bot, obviously!"
+)
+
+def unknown():
+    response = (
+        "I'm not sure what you're trying to say. "
+        "Could you please rephrase or ask something else?"
+    )
+    return response
